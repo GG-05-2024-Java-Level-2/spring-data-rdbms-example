627ec702d5b163d90a7087b46e9a53abbee99ab6
pipzgutz <pmgutierrez@pm.me>
2024-05-28 23:33:36 +0800

    init

---
+++ src/main/java/net/groundgurus/level2/spring_data_rdbms_example/SpringDataRdbmsExampleApplication.java
@@ -0,0 +1,13 @@
+package net.groundgurus.level2.spring_data_rdbms_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRdbmsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRdbmsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/groundgurus/level2/spring_data_rdbms_example/SpringDataRdbmsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.groundgurus.level2.spring_data_rdbms_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRdbmsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
